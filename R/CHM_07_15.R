f86ef80ef2977d2a51f2d81ddc8ee3b40ef6d6d4
ykarna <ykarna@student.unimelb.edu.au>
2017-02-17 11:59:24 +1100

    Added the process and fucntion for DTM, DSM or CSM and CHM

--- R/CHM_07_15.R
+++ R/CHM_07_15.R
@@ -18,3 +18,23 @@ summary(lidar15)
 plot(lidar15)
 dtm15 = grid_terrain(lidar15)
 plot(dtm15)
+plot3d(dtm15)
+
+canopy15 = grid_canopy(lidar15, res = 0.5) ## Using the local maximum algorithm, assigns the elevation of the highest 
+                                           # return within each grid cell to the grid cell center
+plot(canopy15)
+
+lnorm = normalize(lidar15, dtm15)
+plot(lnorm)
+
+chm15 = grid_canopy(lnorm, res = 0.5, start = c(0, 0))
+plot(chm15)
+
+chm15 = as.raster(chm15)
+plot(chm15)
+
+# Canopy surface model: Local maximum algorithm with a resolution of 2 meters
+lidar15 %>% grid_canopy(2) %>% plot
+
+
+
